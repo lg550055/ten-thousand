46fa580e2a7587b6dac87692ebb9b093f9f79ca8
Matt Rangel <mattrangel@gmail.com>
2022-04-25 14:38:28 -0700

    Completed roll dice static method

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,12 +1,13 @@
 import random
 
-class GameLogic():
-  # def __init__():
-    pass
+class GameLogic:
+  # def __init__(self,n):
+  #   self.n = n
 
-@staticmethod
-def roll_dice(n):
+  @staticmethod
+  def roll_dice(n):
     temp = []
     for i in range(n):
       temp.append(random.randint(1,6))
     return tuple(temp)
+
