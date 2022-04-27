7f3d545ec345f5963e6bf6160a2ae1ce3b199f51
polo <lg550055@hotmail.com>
2022-04-26 21:07:26 -0500

    Passes all lab 07 tests

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,6 +1,5 @@
-from telnetlib import GA
-from urllib import response
 from ten_thousand.game_logic import GameLogic
+from ten_thousand.banker import Banker
 
 class Game:
   def __init__(self):
@@ -9,17 +8,43 @@ class Game:
   def play(self, roller=GameLogic.roll_dice):
     print('Welcome to Ten Thousand')
     print('(y)es to play or (n)o to decline')
-    response = input('> ')
-    if response == 'n':
+    prompt = input('> ')
+    if prompt == 'n':
       print('OK. Maybe another time')
-    else:    
-      print('Starting round 1') # substitute 1 for variable
-      print('Rolling 6 dice...') # substitute 6 for variable
-      roll = ''
-      for i in range(6):
-        roll += str(roller(6)) + ' '
-      print(f'*** {roll}***')
-      print('Enter dice to keep, or (q)uit:')
-      response = input('> ')
-      if response == 'q':
-        print('Thanks for playing. You earned 0 points') # substitute 0 for variable
+    else:
+      turn = 1
+      player1 = Banker()
+      while True:
+        dice = 6
+        print(f'Starting round {turn}')
+        print(f'Rolling {dice} dice...')
+        roll_string = ''
+        roll = roller(dice)
+        for e in roll:
+          roll_string += str(e) + ' '
+        print(f'*** {roll_string}***')
+        print('Enter dice to keep, or (q)uit:')
+        prompt = input('> ')
+        if prompt == 'q':
+          break
+        else:
+          dice -= len(prompt)
+          roll_tuple = tuple([int(ch) for ch in prompt])
+          points = GameLogic.calculate_score(roll_tuple) # needs work
+          player1.shelf(points)
+          print(f'You have {player1.shelved} unbanked points and {dice} dice remaining')
+          print('(r)oll again, (b)ank your points or (q)uit:')
+          prompt = input('> ')
+          if prompt == 'q':
+            break
+          if prompt == 'b':
+            print(f'You banked {player1.shelved} points in round {turn}') # sub 1 for variable
+            player1.bank()
+            print(f'Total score is {player1.balance} points')
+            turn += 1
+      
+      print(f'Thanks for playing. You earned {player1.balance} points')
+
+if __name__ == "__main__":
+  game = Game()
+  game.play()
